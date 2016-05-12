59e50d18128a6e176648fc6bbff279f35d5a3037
Rolands Strakis <roland.strakis@gmail.com>
2016-05-12 08:55:05 +0300

    Added sorting, filtering, paging

--- RPGSite/Controllers/ShopController.cs
+++ RPGSite/Controllers/ShopController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Web.Mvc;
 using System.Data.Entity;
+using PagedList;
 
 namespace RPGSite.Controllers
 {
@@ -10,10 +11,53 @@ namespace RPGSite.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Equipments
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.TitleSortParam = string.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            ViewBag.RaritySortParam = sortOrder == "Rarity" ? "rarity_desc" : "Rarity";
+            ViewBag.TypeSortParam = sortOrder == "Type" ? "type_desc" : "Type";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+
             var equipment = db.Equipment.Include(e => e.Rarity).Include(e => e.Type);
-            return View(equipment.ToList());
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                equipment = equipment.Where(e => e.Title.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    equipment = equipment.OrderByDescending(e => e.Title);
+                    break;
+                case "Rarity":
+                    equipment = equipment.OrderBy(e => e.Rarity.Rarity);
+                    break;
+                case "rarity_desc":
+                    equipment = equipment.OrderByDescending(e => e.Rarity.Rarity);
+                    break;
+                case "Type":
+                    equipment = equipment.OrderBy(e => e.Type.Type);
+                    break;
+                case "type_desc":
+                    equipment = equipment.OrderByDescending(e => e.Type.Type);
+                    break;
+                default:
+                    equipment = equipment.OrderBy(e => e.Title);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(equipment.ToPagedList(pageNumber, pageSize));
         }
 
         protected override void Dispose(bool disposing)
