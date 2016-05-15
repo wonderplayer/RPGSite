f6835ef71a809bc2a8c5bf444e47c2fd4e97fad2
Rolands Strakis <roland.strakis@gmail.com>
2016-05-15 18:32:42 +0300

    Events

---
+++ RPGSite/Controllers/EventsController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RPGSite.Models;
+using Microsoft.AspNet.Identity;
+
+namespace RPGSite.Controllers
+{
+    public class EventsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Events
+        public ActionResult Index()
+        {
+            var events = db.Events.Include(e => e.User);
+            return View(events.ToList());
+        }
+
+        // GET: Events/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Events events = db.Events.Find(id);
+            ViewBag.Date = events.Updated ?? events.Created;
+            if (events == null)
+            {
+                return HttpNotFound();
+            }
+            return View(events);
+        }
+
+        // GET: Events/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Events/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,Description,StartDate,EndDate,UserID")] Events events)
+        {
+            events.Created = DateTime.Now;
+            events.UserID = User.Identity.GetUserId();
+            if (events.StartDate > events.EndDate)
+            {
+                ViewBag.DateError = "End date should be greater than the starting date";
+                return View(events);
+            }
+            if (ModelState.IsValid)
+            {
+                db.Events.Add(events);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(events);
+        }
+
+        // GET: Events/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Events events = db.Events.Find(id);
+            if (events == null)
+            {
+                return HttpNotFound();
+            }
+            return View(events);
+        }
+
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,Description,Created,StartDate,EndDate,UserID")] Events events)
+        {
+            events.Updated = DateTime.Now;
+            if (events.StartDate > events.EndDate)
+            {
+                ViewBag.DateError = "End date should be greater than the starting date";
+                return View(events);
+            }
+            if (ModelState.IsValid)
+            {
+                db.Entry(events).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(events);
+        }
+
+        // GET: Events/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Events events = db.Events.Find(id);
+            if (events == null)
+            {
+                return HttpNotFound();
+            }
+            return View(events);
+        }
+
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Events events = db.Events.Find(id);
+            db.Events.Remove(events);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
