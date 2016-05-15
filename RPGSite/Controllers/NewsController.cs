7c9b5b2331cbcf423966bf3f6efe588fd0f7dabe
Rolands Strakis <roland.strakis@gmail.com>
2016-05-15 17:13:26 +0300

    Added news

--- RPGSite/Controllers/NewsController.cs
+++ RPGSite/Controllers/NewsController.cs
@@ -8,56 +8,43 @@ using System.Web;
 using System.Web.Mvc;
 using RPGSite.Models;
 using Microsoft.AspNet.Identity;
-using RPGSite.ViewModels;
 
 namespace RPGSite.Controllers
 {
-    public class PostsController : Controller
+    public class NewsController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: Posts
+        // GET: News
         public ActionResult Index()
         {
-            var posts = db.Posts.Where(p => p.IsNews == false).Include(p => p.User);
+            var posts = db.Posts.Where(p => p.IsNews == true).Include(p => p.User);
             return View(posts.ToList());
         }
 
-        // Really bad. Need to do something about validation correctly. And ofc change the comments section
-        // GET: Posts/Details/5
-        public ActionResult Details(int? id, bool? errors)
+        // GET: News/Details/5
+        public ActionResult Details(int? id)
         {
-            if (errors != null && errors == true)
-            {
-                ViewBag.Error = "Comment must be less then 255 symbols long";
-            }
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Posts posts = db.Posts.Find(id);
-            posts.Comments = db.Comments.Where(c => c.PostID == id).ToList();
-            PostDetailViewModel post = new PostDetailViewModel
-            {
-                Comments = posts.Comments,
-                Post = posts,
-                Date = posts.Updated ?? posts.Created
-            };
+            ViewBag.Date = posts.Updated ?? posts.Created;
             if (posts == null)
             {
                 return HttpNotFound();
             }
-            return View(post);
+            return View(posts);
         }
 
-        // GET: Posts/Create
-        [Authorize]
+        // GET: News/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Posts/Create
+        // POST: News/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
@@ -72,10 +59,11 @@ namespace RPGSite.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
             return View(posts);
         }
 
-        // GET: Posts/Edit/5
+        // GET: News/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -90,7 +78,7 @@ namespace RPGSite.Controllers
             return View(posts);
         }
 
-        // POST: Posts/Edit/5
+        // POST: News/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
@@ -98,6 +86,7 @@ namespace RPGSite.Controllers
         public ActionResult Edit([Bind(Include = "ID,Title,Description,Created,UserID")] Posts posts)
         {
             posts.Updated = DateTime.Now;
+            posts.IsNews = true;
             if (ModelState.IsValid)
             {
                 db.Entry(posts).State = EntityState.Modified;
@@ -107,7 +96,7 @@ namespace RPGSite.Controllers
             return View(posts);
         }
 
-        // GET: Posts/Delete/5
+        // GET: News/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -123,7 +112,7 @@ namespace RPGSite.Controllers
             return View(posts);
         }
 
-        // POST: Posts/Delete/5
+        // POST: News/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
@@ -142,29 +131,5 @@ namespace RPGSite.Controllers
             }
             base.Dispose(disposing);
         }
-
-        #region My methods
-        [Authorize]
-        [HttpPost]
-        public ActionResult AddComment(Comments comment, FormCollection values)
-        {
-            var isError = true;
-            comment.Created = DateTime.Now;
-            comment.UserID = User.Identity.GetUserId();
-            comment.PostID = int.Parse(values["Post.ID"]);
-            if (ModelState.IsValid)
-            {
-                isError = false;
-                using (var db = new ApplicationDbContext())
-                {
-                    db.Comments.Add(comment);
-                    db.SaveChanges();
-                    
-                }
-            }
-            return RedirectToAction("Details", new { id = comment.PostID, errors = isError });
-
-        }
-        #endregion
     }
 }
